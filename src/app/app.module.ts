41ba2d54c1f28f4ac9ad01b09e02466691428156
azizgm1234 <140641043+azizgm1234@users.noreply.github.com>
2024-02-20 19:29:42 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFComponent } from './FrontOffice/template-f/template-f.component';
+import { NavbarFrontComponent } from './FrontOffice/navbar-front/navbar-front.component';
+import { FooterFrontComponent } from './FrontOffice/footer-front/footer-front.component';
+import { HomeFrontComponent } from './FrontOffice/home-front/home-front.component';
+import { SidebarBackComponent } from './BackOffice/sidebar-back/sidebar-back.component';
+import { NavbarBackComponent } from './BackOffice/navbar-back/navbar-back.component';
+import { TemplateBComponent } from './BackOffice/template-b/template-b.component';
+import { HomeBackComponent } from './BackOffice/home-back/home-back.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFComponent,
+    NavbarFrontComponent,
+    FooterFrontComponent,
+    HomeFrontComponent,
+    SidebarBackComponent,
+    NavbarBackComponent,
+    TemplateBComponent,
+    HomeBackComponent
   ],
   imports: [
     BrowserModule,
